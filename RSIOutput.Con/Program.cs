fe24578cc81c8d589df845ebfae844ce321116d5
ShugoYoko <yokotnaname@gmail.com>
2023-08-18 18:40:24 +0900

    プロジェクト ファイルを追加します。

---
+++ RSIOutput.Con/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace RSIOutput.Con
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    
+}
+
---
+++ RSIOutput.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace RSIOutput.Tests
+{
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
