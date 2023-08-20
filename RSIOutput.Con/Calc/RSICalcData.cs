c908ff2f01ade7cfc85ab070866484273c70fc96
ShugoYoko <ShugoYoko@users.noreply.github.com>
2023-08-20 13:28:29 +0900

    RSI計算ロジックの追加

---
+++ RSIOutput.Con/Calc/RSICalcData.cs
@@ -0,0 +1,63 @@
+﻿using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("RSIOutput.Tests")]
+
+namespace RSIOutput.Con.Calc
+{
+    internal class RSICalcData
+    {
+        private Dictionary<DateTime, int> _baseData;
+
+        private List<RSIData> _rsiList;
+
+        public RSICalcData(Dictionary<DateTime, int>? _readData)
+        {
+            this._baseData = _readData;
+            this._rsiList = new List<RSIData>();
+            AddDiff();
+        }
+
+        private void AddDiff()
+        {
+            var orderBaseData = _baseData.OrderBy(x => x.Key.Date);
+            int prePrice = 0;
+            foreach (var item in orderBaseData)
+            {
+                if (prePrice == 0)
+                {
+                    _rsiList.Add(new RSIData { Date = item.Key, Price = item.Value, Diff = 0 });
+
+                }
+                else
+                {
+                    _rsiList.Add(new RSIData { Date = item.Key, Price = item.Value, Diff = item.Value - prePrice });
+
+                }
+                prePrice = item.Value;
+
+            }
+
+        }
+
+        public IEnumerable<RSIData> ReturnRSIDate()
+        {
+
+            foreach (var data in _rsiList)
+            {
+                var before14Date = data.Date.AddDays(-14);
+                var during14 = _rsiList.Where(x => before14Date <= x.Date && x.Date <= data.Date);
+                var plusDiff = during14.Where(x => x.Diff > 0).Sum(x => x.Diff);
+                var minusDiff = (-1) * during14.Where(x => x.Diff < 0).Sum(x => x.Diff);
+
+                if (plusDiff == 0 && minusDiff == 0)
+                {
+                    data.RSI = 0;
+                }
+                else
+                {
+                    data.RSI = plusDiff / (plusDiff + minusDiff) * 100;
+                }
+                yield return data;
+            }
+        }
+    }
+}
---
+++ RSIOutput.Tests/RSICalcData.Test.cs
@@ -0,0 +1,37 @@
+﻿using RSIOutput.Con.Calc;
+
+namespace RSIOutput.Tests
+{
+    public class RSICalcDataTest
+    {
+        [Fact]
+        public void RSICalc()
+        {
+            var dayData = new Dictionary<DateTime, int>();
+            dayData.Add(new DateTime(2023, 1, 1), 1000);
+            dayData.Add(new DateTime(2023, 1, 2), 1020);
+            dayData.Add(new DateTime(2023, 1, 3), 1010);
+            dayData.Add(new DateTime(2023, 1, 4), 1030);
+            dayData.Add(new DateTime(2023, 1, 5), 1040);
+            dayData.Add(new DateTime(2023, 1, 6), 1050);
+            dayData.Add(new DateTime(2023, 1, 7), 1080);
+            dayData.Add(new DateTime(2023, 1, 8), 1070);
+            dayData.Add(new DateTime(2023, 1, 9), 1050);
+            dayData.Add(new DateTime(2023, 1, 10), 1090);
+            dayData.Add(new DateTime(2023, 1, 11), 1100);
+            dayData.Add(new DateTime(2023, 1, 12), 1120);
+            dayData.Add(new DateTime(2023, 1, 13), 1110);
+            dayData.Add(new DateTime(2023, 1, 14), 1120);
+            dayData.Add(new DateTime(2023, 1, 15), 1100);
+
+            var datas = new RSICalcData(dayData);
+            foreach (var data in datas.ReturnRSIDate())
+            {
+                if (data.Date == new DateTime(2023, 1, 15))
+                {
+                    Assert.Equal("2023/01/15,1100,-20.00,70.83", data.ToString());
+                }
+            }
+        }
+    }
+}
